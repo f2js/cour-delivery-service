dad912fb1ca783d387138ce763c754907c4c9b53
josefpp <56867995+josefpp@users.noreply.github.com>
2022-12-08 17:41:01 +0100

    Update deliveryRouter.js

--- Routes/deliveryRouter.js
+++ Routes/deliveryRouter.js
@@ -7,14 +7,15 @@ const {
   getOrdersReadyForPickup,
   acceptOrder,
   rejectOrder,
+  getAllUsers,
 } = require("../Controllers/deliveryController");
 const JWTverify = require("../Middleware/verifyToken");
 
 const USE_AUTH = !process.env["SKIP_AUTH"];
-
+/*
 if (USE_AUTH) {
   router.use(JWTverify);
-}
+}*/
 
 router.route("/ordersReady").post(getOrdersReadyForPickup);
 router.route("/ordersReady/accept").post(acceptOrder);
@@ -22,5 +23,6 @@ router.route("/ordersReady/accept").post(acceptOrder);
 router.route("/orderPickedUp").post(orderPickedUp);
 router.route("/orderDelivered").post(orderDelivered);
 router.route("/orderRejected").post(rejectOrder);
+router.route("/allUsers").get(getAllUsers);
 
 module.exports = router;
